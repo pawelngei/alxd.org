256cb39dfbeee8907ef9caee90490ad0416e0f04
ALXD <github@alxd.org>
2023-07-04 21:34:55 +0200

    add Miesiecznik Znak blogpost

---
+++ content/44pl_solarpunk_w_miesieczniku_znak.md
@@ -0,0 +1,13 @@
+Title: Solarpunk w Miesięczniku ZNAK
+Date: 2023-07-04
+Slug: solarpunk-w-miesieczniku-znak
+Lang: pl
+Translation: false
+opengraph_image: 44_miesiecznik_znak/okladka.jpg
+
+![Miesięcznik Znak 07-08 2023](/images/44_miesiecznik_znak/okladka.jpg)
+<figcaption>Okładka "Miesięcznika Znak" z lipca/sierpnia 2023</figcaption>
+
+Na łamach wakacyjnej edycji "Miesięcznika Znak" ukazał się mój esej: [Solarpunk: przypominając sobie nadzieję](https://www.miesiecznik.znak.com.pl/solarpunk-przypominajac-sobie-nadzieje/). Dostępny jest on zarówno w wersji papierowej, jak i elektronicznej - za paywallem. Oprócz aspektów ruchu o których pisałem wcześniej porusza on także tematykę przepracowywania traum w kulturze - oraz różnic między solarpunkiem a [longtermizmem](https://en.wikipedia.org/wiki/Longtermism).
+
+Niezależnie od publikacji ruszyła też strona [solarpunk.pl](https://solarpunk.pl/), zbierająca wszystkie polskie materiały o ruchu!
--- pelicanconf.py
+++ pelicanconf.py
@@ -3,63 +3,67 @@
 from __future__ import unicode_literals
 from datetime import datetime
 
-AUTHOR = 'Pawel Ngei'
-SITENAME = 'alxd - solarpunk hacker'
-SITETITLE = 'alxd'
-SITESUBTITLE = u'solarpunk | hacker'
+AUTHOR = "Pawel Ngei"
+SITENAME = "alxd - solarpunk hacker"
+SITETITLE = "alxd"
+SITESUBTITLE = "solarpunk | hacker"
 
 # SITEURL = 'https://alxd.org'
-SITEURL = 'localhost:8000'
-SITELOGO = 'images/logo-circ.png'
-FAVICON = 'images/favicon.ico'
+SITEURL = "localhost:8000"
+SITELOGO = "images/logo-circ.png"
+FAVICON = "images/favicon.ico"
 
-BROWSER_COLOR = '#333333'
-PYGMENTS_STYLE = 'monokai'
+BROWSER_COLOR = "#333333"
+PYGMENTS_STYLE = "monokai"
 
 USE_FOLDER_AS_CATEGORY = False
 MAIN_MENU = True
 HOME_HIDE_TAGS = True
 USE_GOOGLE_FONTS = False
 
-MENUITEMS = (('Archives', '/archives.html'),)
+MENUITEMS = (
+    ("Archives", "/archives.html"),
+    ("Glider Ink", "https://glider.ink/"),
+    ("solarpunk.pl", "https://solarpunk.pl/"),
+)
 
 SOCIAL = (
-    ('envelope', 'mailto:alxd (AT) alxd.org'),
-    ('mastodon', 'https://writing.exchange/@alxd'),
-    ('github', 'https://github.com/pawelngei'),
-    ('twitter', 'https://twitter.com/pawelngei'),
-    ('linkedin', 'https://www.linkedin.com/in/paul-ngei-19227983'),
-    ('rss', 'feeds/all.atom.xml'),
+    ("envelope", "mailto:alxd (AT) alxd.org"),
+    ("mastodon", "https://writing.exchange/@alxd"),
+    ("github", "https://github.com/pawelngei"),
+    ("twitter", "https://twitter.com/pawelngei"),
+    ("linkedin", "https://www.linkedin.com/in/paul-ngei-19227983"),
+    ("rss", "feeds/all.atom.xml"),
 )
 
 CC_LICENSE = {
-    'name': 'Creative Commons Attribution-ShareAlike',
-    'version': '4.0',
-    'slug': 'by-sa',
-    'local_icons': True
+    "name": "Creative Commons Attribution-ShareAlike",
+    "version": "4.0",
+    "slug": "by-sa",
+    "local_icons": True,
 }
 
 COPYRIGHT_YEAR = datetime.now().year
 
-TIMEZONE = 'Europe/Warsaw'
+TIMEZONE = "Europe/Warsaw"
 
-I18N_TEMPLATES_LANG = 'en'
-DEFAULT_LANG = 'en'
-OG_LOCALE = 'en_US'
-LOCALE = 'en_US'
+I18N_TEMPLATES_LANG = "en"
+DEFAULT_LANG = "en"
+OG_LOCALE = "en_US"
+LOCALE = "en_US"
 
 # Feed generation is usually not desired when developing
-FEED_ALL_ATOM = 'feeds/all.atom.xml'
+FEED_ALL_ATOM = "feeds/all.atom.xml"
 CATEGORY_FEED_ATOM = None
 TRANSLATION_FEED_ATOM = None
 
-PATH = 'content'
-STATIC_PATHS = ['images', 'slides', 'content/slides', 'scripts']
+PATH = "content"
+STATIC_PATHS = ["images", "slides", "content/slides", "scripts"]
 
 THEME = "themes/Flex"
 
-PLUGIN_PATHS = ['pelican-plugins']
-PLUGINS = ['post_stats']
+PLUGIN_PATHS = ["pelican-plugins"]
+PLUGINS = ["post_stats"]
 
 DISPLAY_PAGES_ON_MENU = True
 DEFAULT_PAGINATION = 5
